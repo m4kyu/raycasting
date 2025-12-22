2e227c9406bfad6de3af67a46128d9b337c714e8
m4kyu <m4kyu@proton.me>
2025-12-22 22:16:38 +0200

    fixed side moving and cleaned up code a litle bit

--- main.c
+++ main.c
@@ -1,46 +1,66 @@
 #include <raylib.h>
 #include <math.h>
+#include <stdio.h>
+
+#define DEGR2RAD PI / 180
+#define FOV 60 
+#define FPS 60
+#define SPEED 500
+#define ROTATION_SPEED 100
+
+#define CELL_SIZE 100
+#define MAP_WIDTH 10 
+#define MAP_HIGHT 10
+
+#define WIDTH 1980
+#define HIGHT 1280
 
 
+Vector2 player = {110, 110};
+Vector2 direction;
+float rotation = 0;
 
-float pos_x = 110;
-float pos_y = 110;  
-int rotation = 0;
-float dx = 0;
-float dy = 1;
-
-int map[][5] = {
-  {1, 1, 1, 1, 1},
-  {1, 0, 1, 0, 1},
-  {1, 0, 0, 0, 1},
-  {1, 0, 1, 0, 1},
-  {1, 1, 1, 1, 1},
+int map[MAP_HIGHT][MAP_HIGHT] = {
+  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  {1, 0, 1, 0, 1, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 1, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 1, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 1, 0, 0, 0, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 0, 0, 0, 1, 1, 1, 0, 1},
+  {1, 0, 1, 0, 1, 0, 0, 1, 0, 1},
+  {1, 0, 1, 0, 0, 0, 0, 1, 0, 1},
+  {1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
 };
 
 
 void drawPlayer();
-void move();
+void move(float fram_time);
 void castRays(int fov);
 void drawMap();
 
 
 
 int main() {
-  InitWindow(500 * 2, 500, "raycasting");
-  SetTargetFPS(60);
+  SetConfigFlags(FLAG_VSYNC_HINT);
+  InitWindow(WIDTH, HIGHT, "raycasting");
+//  SetTargetFPS(FPS);
 
-    dy = sin(rotation * PI / 180);
-    dx = cos(rotation * PI / 180);
+  direction.y = sin(rotation * DEG2RAD);
+  direction.x = cos(rotation * DEG2RAD);
 
+  float frame_time = 0;
   while (!WindowShouldClose()) {
-    move();
+    move(frame_time);
     BeginDrawing();
     ClearBackground(BLACK);
-    drawMap();
-    drawPlayer();
-    castRays(80);
+//    drawMap();
+    castRays(FOV);
+ //   drawPlayer();
+
 
     DrawFPS(0, 0);
+    frame_time = GetFrameTime();
     EndDrawing();
 
   }
@@ -50,113 +70,126 @@ int main() {
 
 
 void drawPlayer() {
-  DrawCircle(pos_x, pos_y, 10, YELLOW);
-  DrawLineEx((Vector2){pos_x, pos_y}, (Vector2){pos_x + 15 * dx, pos_y + 15 * dy}, 3, RED);
+  DrawCircle(player.x, player.y, 10, YELLOW);
+  DrawLineEx((Vector2){player.x, player.y}, (Vector2){player.x + 15 * direction.x, player.y + 15 * direction.y}, 3, RED);
 }
 
 void drawMap() {
   int x = 0, y = 0;
   Color color;
-  for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 5; j++) {
+  for (int i = 0; i < MAP_HIGHT; i++) {
+    for (int j = 0; j < MAP_WIDTH; j++) {
       color = map[i][j] == 0 ? BLACK : BLUE;
 
-      DrawRectangle(x, y, 100, 100, color);
-      x += 100;
+      DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, color);
+      x += CELL_SIZE;
     }
-    y += 100;
+    y += CELL_SIZE;
     x = 0;
   }
 }
 
 void castRays(int fov) {
-  float norm_x = (float)((int)pos_x % 100) / 100; 
-  float norm_y = (float)((int)pos_y % 100) / 100;
+  float norm_x = (float)((int)player.x % CELL_SIZE) / CELL_SIZE; 
+  float norm_y = (float)((int)player.y % CELL_SIZE) / CELL_SIZE;
 
 
-  float step1 = (float)fov / 500;
-  float start = -(float)fov / 2;
-  for (int i = 0, j = 0; i < 500; i++, j++, start += step1) {
-      int index_x = pos_x / 100;
-  int index_y = pos_y / 100;
+  float step = (float)fov / WIDTH;
+  float deltaA = -(float)fov / 2;
 
-    float diry = sin((rotation + start) * PI / 180); 
-    float dirx = cos((rotation + start) * PI / 180); 
+  for (int i = 0, j = 0; i < WIDTH; i++, j++, deltaA += step) {
+    int index_x = player.x / CELL_SIZE;
+    int index_y = player.y / CELL_SIZE;
+    float diry = sin((rotation + deltaA) * DEG2RAD); 
+    float dirx = cos((rotation + deltaA) * DEG2RAD); 
     int cor_x = index_x, cor_y = index_y;
 
-    Vector2 step = {sqrtf(1 + (diry / dirx) * (diry / dirx)), sqrtf(1 + (dirx / diry) * (dirx / diry))};
+    Vector2 deltaStep = {sqrtf(1 + (diry / dirx) * (diry / dirx)), sqrtf(1 + (dirx / diry) * (dirx / diry))};
     Vector2 stepdir = {0};
     Vector2 rayLen = {0};
 
     if (dirx < 0) {
 			stepdir.x = -1;
-			rayLen.x = norm_x * step.x;
+			rayLen.x = norm_x * deltaStep.x;
 		} else {
 			stepdir.x = 1;
-			rayLen.x =  (1 - norm_x) * step.x;
+			rayLen.x =  (1 - norm_x) * deltaStep.x;
 		}
 
 
 	  if (diry < 0) {
 			stepdir.y = -1;
-			rayLen.y = norm_y * step.y;
+			rayLen.y = norm_y * deltaStep.y;
 		} else {
 			stepdir.y = 1;
-		  rayLen.y = (1 - norm_y) * step.y;
+		  rayLen.y = (1 - norm_y) * deltaStep.y;
 		}
 
 
     int hit = 0;
 		float distance = 0.0f;
-		while (!hit)  {
+    float maxd = 1000.0f;
+
+    Color color;
+    int side;
+		while (!hit && distance < maxd)  {
 			if (rayLen.x < rayLen.y) {
 				index_x += stepdir.x;
 				distance = rayLen.x;
-				rayLen.x += step.x;
+				rayLen.x += deltaStep.x;
+        color = GREEN;
+        side = 0;
 			} else {
 				index_y += stepdir.y;
 				distance = rayLen.y;
-				rayLen.y += step.y;
+				rayLen.y += deltaStep.y;
+        color = BLUE;
+        side = 1;
 			}
 
-			if (index_x >= 0 && index_x < 5 && index_y >= 0 && index_y < 5) {
+			if (index_x >= 0 && index_x < MAP_WIDTH && index_y >= 0 && index_y < MAP_HIGHT) {
 				if (map[index_y][index_x] == 1)
 					hit = 1;
 			}
 		}
 
-    float wall_size = 500 / (distance * 3); 
-    distance *= 100;
-    DrawLineEx((Vector2){pos_x, pos_y}, (Vector2){pos_x + distance * dirx, pos_y + distance * diry}, 1, GREEN);
-    DrawLineEx((Vector2){500 + (j), 250 - (wall_size / 2)}, (Vector2){500 + (j), 250 + (wall_size / 2)}, 1, GREEN);
+    float perpWallDist;
+    if(side == 0) perpWallDist = (rayLen.x - deltaStep.x);
+    else          perpWallDist = (rayLen.y - deltaStep.y);
+
+
+  //  DrawLineEx((Vector2){player.x, player.y}, (Vector2){player.x + distance * CELL_SIZE * dirx, player.y + distance * CELL_SIZE * diry}, 1, GREEN);
+
+    float wall_size = HIGHT / (perpWallDist); 
+    DrawLineEx((Vector2){(j), ((float)HIGHT / 2) - (wall_size / 2)}, (Vector2){(j), ((float)HIGHT / 2) + (wall_size / 2)}, 1, color);
   }
 }
 
-void move() {
+void move(float frame_time) {
   if (IsKeyDown(KEY_W)) {
-    pos_y += 1 * dy;
-    pos_x += 1 * dx;
+    player.y += SPEED * direction.y * frame_time;
+    player.x += SPEED * direction.x * frame_time;
   } else if (IsKeyDown(KEY_S)) {
-    pos_y -= 1 * dy;
-    pos_x -= 1 * dx;
+    player.y -= SPEED * direction.y * frame_time;
+    player.x -= SPEED * direction.x * frame_time;
   }
 
   if (IsKeyDown(KEY_A)) {
-    pos_x += 1 * dy;
-    pos_y += 1 * dx;
+    player.x += SPEED * direction.y * frame_time;
+    player.y += SPEED * -direction.x * frame_time;
   } else if (IsKeyDown(KEY_D)) {
-    pos_x -= 1 * dy;
-    pos_y -= 1 * dx;
+    player.x += SPEED * -direction.y * frame_time;
+    player.y += SPEED * direction.x * frame_time;
   }
 
   if (IsKeyDown(KEY_Q)) {
-    rotation--;
-    dy = sin(rotation * PI / 180);
-    dx = cos(rotation * PI / 180);
+    rotation -= ROTATION_SPEED * frame_time;
+    direction.y = sin(rotation * PI / 180);
+    direction.x = cos(rotation * PI / 180);
   } else if (IsKeyDown(KEY_E)) {
-    rotation++;
-    dy = sin(rotation * PI / 180);
-    dx = cos(rotation * PI / 180);
+    rotation += ROTATION_SPEED * frame_time;
+    direction.y = sin(rotation * PI / 180);
+    direction.x = cos(rotation * PI / 180);
   }
 }
 
